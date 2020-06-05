224407f12d0495edcbf5d0a20718393f0f89a839
Hera Hassan <hehassa1@in.ibm.com>
2020-06-05 16:39:32 +0530

    initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
-// import { TasksService } from '../tasks.service';
-import { HttpClient } from '@angular/common/http';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-home',
@@ -10,22 +9,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent {
   errMessage: string;
-  public task: Task;
-  public tasks: Task[];
+  public tasks: any;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private taskService : TaskService) { }
 
   getTasks(){
-    // this.tasks=JSON.parse(this.json_data)["task"][Math.floor(Math.random() * 2)];
-    // console.log(this.tasks);
-    this.httpClient.get<any>('assets/db.json',{}).subscribe(data => {
-      this.tasks=data["tasks"][Math.floor(Math.random() * 5)]; // 0-4
-      // this.tasks=data["tasks"][0];
-      // console.log(this.tasks["text"]);
-    });
-    // this.tasksService.getTasks().subscribe(
-    //   data => this.tasks = data,
-    //   err => { this.errMessage = 'Http failure response for http://localhost:4200/task: 404 Not Found'; }
-    // );
+    this.taskService.apiCall().subscribe(
+      data => this.tasks = data,
+      err => { this.errMessage = 'Http failure response :: 404 Not Found'; }
+    );
   }
 }
